fc37cfbf40266aeea5a8b508e0d6e978eafddd2d
James Karnes <karnes.james@gmail.com>
2020-10-22 13:50:00 -0400

    Changed settings for github

--- diveshop/settings.py
+++ diveshop/settings.py
@@ -132,8 +132,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'liceo.matias.ramon.mella.bavaro@gmail.com'
-EMAIL_HOST_PASSWORD = 'RamonMella'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 django_heroku.settings(locals())
